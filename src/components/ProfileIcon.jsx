792d4dd2635943ca1ec19e445084685a5d98942c
Sayakdutt <sayakofficial01@gmail.com>
2023-11-08 20:30:29 +0530

    removed 2 unused imports

--- src/components/ProfileIcon.jsx
+++ src/components/ProfileIcon.jsx
@@ -1,7 +1,5 @@
 import { TbGridDots } from "react-icons/tb";
 
-import Profile from "../assets/profile-200x200.jpg";
-import ProfileRing from "../assets/profile-ring.svg";
 
 const ProfileIcon = () => {
     return (
