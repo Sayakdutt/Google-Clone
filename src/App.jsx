50e982887ba4c556076c56cb0d98221815a3a34f
Sayakdutt <sayakofficial01@gmail.com>
2023-12-04 21:06:21 +0530

    changed the path of context

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import SearchResult from "./components/SearchResult";
-import { AppContext } from "./context/imageSearchContext";
+import { AppContext } from "./context/ImageSearchContext";
 import Error from "./components/Error";
 
 function App() {
